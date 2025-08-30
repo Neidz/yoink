fc9514ed85f7aa1774b82ec66baf85284c892aed
Neidz <adamo7255@gmail.com>
2025-08-30 15:40:27 +0200

    move encoding to separate file

---
+++ src/encoding.rs
@@ -0,0 +1,10 @@
+pub fn urlencode(val: &str) -> String {
+    val.bytes()
+        .map(|b| match b {
+            b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'-' | b'_' | b'.' | b'~' => {
+                (b as char).to_string()
+            }
+            _ => format!("%{:02X}", b),
+        })
+        .collect()
+}
